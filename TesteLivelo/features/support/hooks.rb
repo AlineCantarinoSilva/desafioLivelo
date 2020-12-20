5b076cb750c4cc01cd2aa74082a53daadbd9f558
Aline Cantarino <aline.sistemasinternet@gmail.com>
2020-12-20 16:01:27 -0300

    alterações

--- TesteLivelo/features/support/hooks.rb
+++ TesteLivelo/features/support/hooks.rb
@@ -7,7 +7,6 @@ Before do
     page.current_window.resize_to(1366, 768)
 end
 
-
 After do |scenario|
     nome_cenario = scenario.name.gsub(/[^A-Za-z0-9 ]/, '')
     nome_cenario = nome_cenario.gsub(' ', '_').downcase!    
